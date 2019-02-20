5d2885f994e2e45d2903e9e9930ebc716d09ed55
gauarv-123 <gauravladhani4@gmail.com>
2019-02-20 15:16:46 +0530

    Dev Branch

--- src/A.java
+++ src/A.java
@@ -3,7 +3,13 @@ public class A {
 
 	public static void main(String[] args) {
 		
-		System.err.println("HELLO  JAVA BHAIYA");
+		int a=10;
+		int b=10;
+		int c=a+b;
+		
+		System.err.println("Sum");
+		
+		
 	
 	}
 
